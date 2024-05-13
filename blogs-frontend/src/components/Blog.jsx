f357ea018aa0c7476284e7e4b3572350c6f3ee02
valetzu <valzerlazer@gmail.com>
2024-05-13 14:09:54 +0300

    show more info toggle for blog post feature

--- blogs-frontend/src/components/Blog.jsx
+++ blogs-frontend/src/components/Blog.jsx
@@ -1,19 +1,40 @@
+import { useState } from 'react'
 const Blog = ({blog, deleteHandler}) => {
-    return (
-      <li key={blog.id}>
+
+  const [showMoreInfo, setShowMoreInfo] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  } 
+  const moreInfo = () => {return (
+    <div>
+      <p>{`URL: ${blog.url}`}</p>
+      <p>Likes: {blog.likes} <button>Like</button></p>
+    </div>
+  )} 
+  return (
+      <div style={blogStyle}>
         {
         <div>
           
-          <h2>{blog.title}</h2>
+          <h2>{`${blog.title} - ${blog.author}`}</h2>
           
-          <span>
-            <p>{`Author: ${blog.author}, URL: ${blog.url}`}
+          <div>
+            <button onClick={() => {setShowMoreInfo(!showMoreInfo)}}>More info</button>
             <button onClick={deleteHandler}>Delete Blog post</button>
-            </p>
-          </span>
+            {showMoreInfo ?
+            moreInfo()
+            :
+            null
+            }
+            
+          </div>
         </div>
          }
-      </li>
+      </div>
     )
   }
   
