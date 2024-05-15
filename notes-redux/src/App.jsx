7acf8e00d7104371f668ea24a3af099ce8fdf6cb
valetzu <valzerlazer@gmail.com>
2024-05-15 10:48:56 +0300

    separated logic into own modules

--- notes-redux/src/App.jsx
+++ notes-redux/src/App.jsx
@@ -1,9 +1,8 @@
 import { createNote, toggleImportanceOf } from './reducers/noteReducer'
 import { useSelector, useDispatch } from 'react-redux'
-
+import Notes from './components/Notes'
 
 const App = () => {
-
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
 
@@ -11,12 +10,10 @@ const App = () => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-
     dispatch(createNote(content))
   }
 
   const toggleImportance = (id) => {
-
     dispatch(toggleImportanceOf(id))
   }
 
@@ -26,17 +23,7 @@ const App = () => {
         <input name="note" /> 
         <button type="submit">add</button>
       </form>
-      <ul>
-
-        {notes.map(note =>
-          <li
-            key={note.id} 
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <Notes />
     </div>
   )
 }
