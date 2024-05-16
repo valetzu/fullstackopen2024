00019f26eb6188f4eb6aab9837dd8955f65a0452
valetzu <valzerlazer@gmail.com>
2024-05-16 18:52:12 +0300

    Anecdote app react routing experiment

---
+++ routed-anecdotes/src/App.jsx
@@ -0,0 +1,119 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import About from './components/About'
+import Footer from './components/Footer'
+import AnecdoteList from './components/AnecdoteList'
+
+const Menu = (props) => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <Router>
+    <div>
+      <Link style={padding} to="/anecdotes">anecdotes</Link>
+      <Link style={padding} to="/create">create a new</Link>
+      <Link style={padding} to="/about">about</Link>
+    </div>
+
+    <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={props.anecdotes} />} />
+        <Route path="/anecdotes" element={<AnecdoteList anecdotes={props.anecdotes} />} />
+        <Route path="/create" element={<CreateNew anecdotes={props.anecdotes} addNew={props.addNew}/>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+    </Router>
+  )
+}
+
+const CreateNew = (props) => {
+  const [content, setContent] = useState('')
+  const [author, setAuthor] = useState('')
+  const [info, setInfo] = useState('')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content,
+      author,
+      info,
+      votes: 0
+    })
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <div>
+          author
+          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+        </div>
+        <div>
+          url for more info
+          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu anecdotes={anecdotes} addNew={addNew}/>
+      <i><Footer /></i>
+    </div>
+  )
+}
+
+
+
+export default App
